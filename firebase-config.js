d53808e72e75c4bbf03bbb57e248bc2715f713ca
karata9 <karatalrafden2024@gmail.com>
2026-07-16 17:11:05 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCecUHYY9aNdDjmLsjNMU0JTr7FBv2cuXs",
+    authDomain: "sthmar-7bfd0.firebaseapp.com",
+    projectId: "sthmar-7bfd0",
+    storageBucket: "sthmar-7bfd0.firebasestorage.app",
+    messagingSenderId: "927651931910",
+    appId: "1:927651931910:web:ab383126e843fcf2136087"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
